3530c184f818cee4e91fea78a7e616242cd6feb7
Anheru88 <ajimenez@digitalcaribe.com.co>
2015-05-12 14:10:42 -0500

    First Commit

---
+++ SAGS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SAGS.Startup))]
+namespace SAGS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
