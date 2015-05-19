f260d5d29989522af4cdba8b58ad043d61ac7372
Anheru88 <ajimenez@digitalcaribe.com.co>
2015-05-19 12:11:41 -0500

    ELiminado Document Type, para usar la plantilla

--- SAGS/Models/IdentityModels.cs
+++ SAGS/Models/IdentityModels.cs
@@ -29,7 +29,5 @@ namespace SAGS.Models
         {
             return new ApplicationDbContext();
         }
-
-        public System.Data.Entity.DbSet<BusinessLogic.DocumentType> DocumentTypes { get; set; }
     }
 }
