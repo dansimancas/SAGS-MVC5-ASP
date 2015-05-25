207ee52517dd66bc72e6cfb32c95c8b2e59d3efe
Anheru88 <ajimenez@digitalcaribe.com.co>
2015-05-24 19:30:03 -0500

    Agregados el data Context de Guardian

---
+++ SAGS/DataContexts/GuardianDB.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using SAGS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Text;
+using SAGS.DataContexts.GuardiansMigrations;
+
+namespace SAGS.DataContexts
+{
+    public class GuardianDB : DbContext
+    {
+        public GuardianDB()
+            : base("DefaultConnection")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<GuardianDB, Configuration>());
+        }
+
+        public System.Data.Entity.DbSet<BusinessLogic.Guardian> Guardians { get; set; }
+    }
+}
---
+++ SAGS/DataContexts/GuardiansMigrations/Configuration.cs
@@ -0,0 +1,33 @@
+namespace SAGS.DataContexts.GuardiansMigrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SAGS.DataContexts.GuardianDB>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+            MigrationsDirectory = @"DataContexts\GuardiansMigrations";
+        }
+
+        protected override void Seed(SAGS.DataContexts.GuardianDB context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
