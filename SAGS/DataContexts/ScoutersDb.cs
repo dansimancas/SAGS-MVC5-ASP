964ae8ae54952a67b471c0048fb94e2067259b10
Anheru88 <ajimenez@digitalcaribe.com.co>
2015-05-24 22:31:15 -0500

    Agregado el contexto para Scouters.

---
+++ SAGS/DataContexts/ScoutersDb.cs
@@ -0,0 +1,21 @@
+﻿
+using SAGS.DataContexts.ScoutersMigrations;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SAGS.DataContexts
+{
+    public class ScoutersDb : DbContext
+    {
+        public ScoutersDb()
+            : base("DefaultConnection")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ScoutersDb, Configuration>());
+        }
+
+        public System.Data.Entity.DbSet<BusinessLogic.Scouter> Scouters { get; set; }
+    }
+}
